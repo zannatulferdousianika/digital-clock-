73a56e3a566bf57a466e605483cd912ac2a573cd
zannatulferdousianika <zfanika79@gmail.com>
2025-03-24 19:16:26 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 const clockElement = document.getElementById("clock");
 
+
+
+
+
 function updateClock(){
-    const now = new Date ();
+    const now = new Date();
     let hours = now.getHours();
     const minutes = String(now.getMinutes()).padStart(2,"0")
     const seconds = String(now.getSeconds()).padStart(2,"0");
     const amPM = hours >= 12 ? "PM" : "AM" ;
 
-    hours = hours%12 || 12;
-    clockElement.textContent =`${String(hours),padStart(2,"0")}: ${minutes} : ${seconds}: ${amPM} `;
 
+    hours = hours%12 || 12;
+    clockElement.textContent =`${String(hours).padStart(2,"0")}: ${minutes} : ${seconds}: ${amPM} `;
+
+    clock.textContent = `${dateStr}`;
+
+
+    
     
 
 }
 
 
-setInterval(updateClock);
+setInterval(updateClock,1000);
 updateClock();
 
 
